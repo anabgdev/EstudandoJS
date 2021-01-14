2bf84a689bad4974d98fc1d460dba8dacd1c9983
Ana Carolina Borges <anabg.dev@gmail.com>
2021-01-14 11:35:40 -0300

    Create booleanos.js

---
+++ aula-pratica04/aula-pratica05/booleanos.js
@@ -0,0 +1,16 @@
+const idade = 19;
+const maiorIdade = idade >= 18;
+
+console.log(maiorIdade);
+
+const nome = "lís";
+const nomeIgual = nome === "vitória";
+
+console.log(nomeIgual);
+
+
+const peso = 55.8,
+    altura = 1.65;
+const saudavel = peso < 55 && altura < 1.60;
+
+console.log(saudavel);
