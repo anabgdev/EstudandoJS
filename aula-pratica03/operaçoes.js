81e1c42a77677a2289e09659e68ff663bedfc9f0
Ana Carolina Borges <anabg.dev@gmail.com>
2021-01-13 22:51:11 -0300

    Create operaçoes.js

---
+++ aula-pratica03/operaçoes.js
@@ -0,0 +1,17 @@
+let adicao = 2 + 2;
+divisao = 16 / 2;
+let multip = 15 * 3;
+sub = 100 - 50;
+
+resto = 23 % 6;
+result = 8 - (2 * 90) + (7 / 4);
+
+console.log(`Esse é o resultado da adição: ${adicao}`);
+console.log(`Esse é o resultado da divisão: ${divisao}`);
+console.log(`Esse é o resultado da multiplicação: ${multip}`);
+console.log(`Esse é o resultado da subtração: ${sub}`);
+console.log(`Esse é o resultado do resto: ${resto}`);
+console.log(`Esse é o resultado do result: ${result}`);
+
+resultado = adicao + sub + 80;
+console.log(`Esse é o resultado do resultado: ${resultado}`);
